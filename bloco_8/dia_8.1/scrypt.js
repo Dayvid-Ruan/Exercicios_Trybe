556560620e84655d8b9a4c09ccca48903730104e
Dayvid Ruan <ruan12bbg@gmail.com>
2021-05-25 18:58:24 -0300

    exercicio 1

--- bloco_8/dia_8.1/scrypt.js
+++ bloco_8/dia_8.1/scrypt.js
@@ -1,8 +1,9 @@
-const  acorda = () => 'Acordando!';
-const  cafe = () => 'Bora tomar café!';
-const  dormir = () => 'Partiu dormir!';
+  const  acorda = () => 'Acordando!';
+  const  cafe = () => 'Bora tomar café!';
+  const  dormir = () => 'Partiu dormir!';
 
-const rotina = (str) => str(acorda, cafe, dormir)
-console.log(rotina(acorda));
-console.log(rotina(cafe));
-console.log(rotina(dormir));
+  const rotina = (str) => str(acorda, cafe, dormir)
+
+ console.log(rotina(acorda));
+ console.log(rotina(cafe));
+ console.log(rotina(dormir));
