74a496138514f302dd63be958b1c887c918ab5dc
Dayvid Ruan <ruan12bbg@gmail.com>
2021-05-31 19:12:25 -0300

    Resolve erros exercicio 1

--- bloco_8/dia_8.5/exercicio1.js
+++ bloco_8/dia_8.5/exercicio1.js
@@ -13,4 +13,3 @@ const rectangles = [rectangle1, rectangle2, rectangle3];
 rectangles.forEach((rectangle) => {
   assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
 });
-console.log(rectangleArea)
